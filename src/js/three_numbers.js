7951f058674bca950bc17e6098aced3726c2cd58
NikaMartynchuk <veronika.martynchuk@gmail.com>
2025-03-08 23:37:28 +0200

    //

--- src/js/three_numbers.js
+++ src/js/three_numbers.js
@@ -1,10 +1,10 @@
-function findMax() {
-    let num1 = parseFloat(document.querySelector(".num11").value) || 0;
-    let num2 = parseFloat(document.querySelector(".num22").value) || 0;
-    let num3 = parseFloat(document.querySelector(".num33").value) || 0;
+export function findMax() {
+    let num1 = parseFloat(document.querySelector(".num11").value);
+    let num2 = parseFloat(document.querySelector(".num22").value);
+    let num3 = parseFloat(document.querySelector(".num33").value);
     let resultElement = document.querySelector(".result-one");
     
-    if (document.querySelector(".num11").value && document.querySelector(".num22").value && document.querySelector(".num33").value) {
+    if (!isNaN(".num1") && !isNaN(".num2") && !isNaN(".num3")) {
         let maxNumber = Math.max(num1, num2, num3);
         resultElement.innerText = "Найбільше число, яке ви ввели - " + maxNumber;
     } else {
