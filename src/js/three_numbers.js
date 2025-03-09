dba40bdd71f5529a77d61d7b111ccef31c41cc78
NikaMartynchuk <veronika.martynchuk@gmail.com>
2025-03-09 13:53:45 +0200

    //

--- src/js/three_numbers.js
+++ src/js/three_numbers.js
@@ -4,7 +4,7 @@ export function findMax() {
     let num3 = parseFloat(document.querySelector(".num33").value);
     let resultElement = document.querySelector(".result-one");
     
-    if (!isNaN(".num1") && !isNaN(".num2") && !isNaN(".num3")) {
+    if (!isNaN(num1) && !isNaN(num2) && !isNaN(num3)) {
         let maxNumber = Math.max(num1, num2, num3);
         resultElement.innerText = "Найбільше число, яке ви ввели - " + maxNumber;
     } else {
