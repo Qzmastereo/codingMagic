e824b20f0a8041ca0724ee79702264c40fc2b414
lion4ik9 <burundukleo@gmail.com>
2025-03-13 18:41:42 +0200

    1

---
+++ src/js/stone_game.js
@@ -0,0 +1,14 @@
+//Рахунок:
+//Комп'ютер - 0
+//Ви - 0
+
+let stone = document.getElementById('stone');
+let scissors = document.getElementById('scissors');
+let paper = document.getElementById('paper');
+
+let PCVar = document.getElementById('stone__bottom-mod');
+
+
+let PCAnswer = '';
+let HumanAnser = '';
+
